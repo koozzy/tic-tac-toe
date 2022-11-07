6845924c0ffd1d9988f75bf8af9c96c4999c8380
koozzy <hq2160@columbia.edu>
2022-11-06 22:56:03 -0800

    edited cli.py

--- cli.py
+++ cli.py
@@ -2,16 +2,32 @@
 # the Tic-Tac-Toe game. This is where input and output happens.
 # For core game logic, see logic.py.
 
-from logic import make_empty_board
+from logic import make_empty_board, print_board, input_move, other_player, get_winner, is_valid_input, is_tie
 
 
 if __name__ == '__main__':
     board = make_empty_board()
     winner = None
+    print_board(board)
+    # ask user to input the first player name
+    player = input('Enter Your Chosen Charactor, X or O:')
     while winner == None:
-        print("TODO: take a turn!")
-        # TODO: Show the board to the user.
-        # TODO: Input a move from the player.
-        # TODO: Update the board.
-        # TODO: Update who's turn it is.
-        winner = 'X'  # FIXME
+        row = input('Enter Your Chosen Row,0-2:')
+        col = input('Enter Your Chosen Column,0-2:')
+        while not is_valid_input(board, int(row), int(col)):
+            row = input('Enter Your Chosen Row,0-2:')
+            col = input('Enter Your Chosen Column,0-2:')
+        
+        board = input_move(board, player, int(row), int(col))
+        print_board(board)
+        if is_tie(board) == False:
+            if get_winner(board) == None:
+                print("Take a turn!")
+                player = other_player(player)
+            else:
+                winner = player
+                print(player, "is the winner.")
+        else:
+            print("This is a tie.")
+            break
+        
--- logic.py
+++ logic.py
@@ -2,44 +2,80 @@
 # or output happens here. The logic in this file
 # should be unit-testable.
 
-
 def make_empty_board():
     return [
         [None, None, None],
         [None, None, None],
         [None, None, None],
     ]
-board = make_empty_board()
-row_len = len(board)
-col_len = len(board[0])
 
 def get_winner(board):
     """Determines the winner of the given board.
     Returns 'X', 'O', or None."""
-    found_winner = False
+    row_len = len(board)
+    col_len = len(board[0])
 
     for i in range(row_len):
         if board[i][0] == board[i][1] == board[i][2]:
-            found_winner = True
             return board[i][0]
     
     for i in range(col_len):
         if board[0][i] == board[1][i] == board[2][i]:
-            found_winner = True
             return board[0][1]
         
     if board[0][0] == board[1][1] == board[2][2]:
-            found_winner = True
-            return board[0][0]
+        return board[0][0]
 
     if board[0][2] == board[1][1] == board[2][0]:
-            found_winner = True
-            return board[0][2]
+        return board[0][2]
 
+    return "None"            
 
 def other_player(player):
     """Given the character for a player, returns the other player."""
-    for i in board[i][col_len]:
-        for j in board[rol_len][j]:
-            if board[i][j] == player:
-                return board[i][j]  # FIXME
+    if player == "X":
+       return "O"
+    else:
+       return "X"
+
+def print_board(board):
+    row_len = len(board)
+    col_len = len(board[0])
+    for i in range(row_len):
+        for j in range(col_len):
+            print(board[i][j], end = ' ')
+        print("", end="\n")
+     
+def input_move(board, player, row, col):
+    if board[row][col] == None:
+        board[row][col] = player
+    return board
+
+def is_valid_input(board, row, col):
+    return board[row][col] == None
+
+def is_tie(board):
+    row_len = len(board)
+    col_len = len(board[0])
+
+    for i in range(row_len):
+        checker_set = set(board[i])
+        if "O" not in checker_set or "X" not in checker_set:
+            return False
+            
+    for j in range(col_len):
+        checker_set = set()
+        for i in range(row_len):
+            checker_set = checker_set.add(board[i][j])
+        if "O" not in checker_set or "X" not in checker_set:
+            return False
+
+    checker_set = set(board[0][0], board[1][1], board[2][2])
+    if "O" not in checker_set or "X" not in checker_set:
+            return False
+
+    checker_set = set(board[0][2], board[1][1], board[2][0])        
+    if "O" not in checker_set or "X" not in checker_set:
+            return False
+
+    return True  
