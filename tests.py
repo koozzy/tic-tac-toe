da2ee9f8ebc7d8b72877f45df6a18d0e86de4d21
koozzy <hq2160@columbia.edu>
2022-11-08 13:37:05 -0800

    modified test.py

--- tests.py
+++ tests.py
@@ -12,6 +12,31 @@ class TestLogic(unittest.TestCase):
         ]
         self.assertEqual(logic.get_winner(board), 'X')
 
+    def other_player(self):
+        player = 'X'
+        self.assertEqual(logic.other_player(player), 'X')
+
+    def print_board(self):
+        board = [
+            ['X', None, 'O'],
+            [None, 'X', None],
+            [None, 'O', 'X'],
+        ]
+        self.assertEqual(logic.print_board(board), 'X')
+    
+    def input_move(board, player, row, col)
+    
+    def is_valid_input(board, row, col)
+
+    def is_tie(self):
+        board = [
+            ['X', None, 'O'],
+            [None, 'X', None],
+            [None, 'O', 'X'],
+        ]
+        self.assertEqual(logic.is_tie(board), 'X')
+
+
     # TODO: Test all functions from logic.py!
 
 
